7cf7b0576adda9559e2e3290bdadc5fa647c457e
alex <lxalonso@gmail.com>
2022-08-01 12:23:35 +0200

    Redux and firs slice configured

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,12 +1,15 @@
-import { BrowserRouter } from "react-router-dom"
-import { AppRouter } from "./router/AppRouter"
-
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./router/AppRouter";
+import  { store } from './store'
 export const CalendarApp = () => {
   return (
     <>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <Provider store={ store }>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
